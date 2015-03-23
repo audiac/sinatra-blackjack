8c2c2b22656ae20666aa3ce166478cadb24e9d02
Ryan <ryan.geo@gmail.com>
2015-03-23 05:42:33 -0700

    Finish adding Ajax calls

--- main.rb
+++ main.rb
@@ -57,20 +57,20 @@ helpers do
     @play_again = true
     @show_hit_or_stay_buttons = false
     session[:player_pot] += session[:player_bet]
-    @success = "<strong>#{session[:player_name]} wins! #{msg}</strong>"
+    @winner = "<strong>#{session[:player_name]} wins! #{msg}</strong>"
   end
 
   def loser!(msg)
     @play_again = true
     @show_hit_or_stay_buttons = false
     session[:player_pot] -= session[:player_bet]
-    @error = "<strong>#{session[:player_name]} loses. #{msg}</strong>"
+    @loser = "<strong>#{session[:player_name]} loses. #{msg}</strong>"
   end
 
   def tie!(msg)
     @play_again = true
     @show_hit_or_stay_buttons = false
-    @success = "<strong>It's a tie! #{msg}</strong>"
+    @winner = "<strong>It's a tie! #{msg}</strong>"
   end
 
   def reveal_dealer_card
@@ -183,7 +183,7 @@ get '/game/dealer' do
     @show_dealer_hit_button = true
   end
 
-  erb :game
+  erb :game, layout: false
 end
 
 post '/game/dealer/hit' do
@@ -204,7 +204,7 @@ get '/game/compare' do
     tie!("#{session[:player_name]} and the Dealer push with #{player_total}.")
   end
 
-  erb :game
+  erb :game, layout: false
 end
 
 get '/game_over' do
